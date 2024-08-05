bd5cc662d81cb2f3f059f5dfe1be7a963854d253
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-08-05 01:16:58 +0100

    cors

--- app.ts
+++ app.ts
@@ -6,7 +6,25 @@ const app: Express = express();
 
 if (ENV === "development") {
   app.use(cors());
-}
+} else
+  app.use(function (req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ["*"];
+    const origin = req.headers.origin!;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-credentials", "true");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, UPDATE"
+    );
+    next();
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
