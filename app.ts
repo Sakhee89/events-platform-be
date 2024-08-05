0d9886fb4b793dde111bd92e4b9ae749019840fb
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-08-05 01:25:06 +0100

    cors

--- app.ts
+++ app.ts
@@ -4,27 +4,7 @@ import cors from "cors";
 const ENV = process.env.NODE_ENV || "development";
 const app: Express = express();
 
-if (ENV === "development") {
-  app.use(cors());
-} else
-  app.use(function (req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = ["*"];
-    const origin = req.headers.origin!;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-    }
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header("Access-Control-Allow-credentials", "true");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, UPDATE"
-    );
-    next();
-  });
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
