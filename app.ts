5735163886abd56c779c9318b258da2bfc2e52c4
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-08-05 01:28:00 +0100

    cors

--- app.ts
+++ app.ts
@@ -4,7 +4,9 @@ import cors from "cors";
 const ENV = process.env.NODE_ENV || "development";
 const app: Express = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://fe-events-platform.vercel.app"
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
